8a3b4cdcd5092928785662b7aa8340d629684c58
KanarUIB <1810275@stud.hs-mannheim.de>
2020-11-03 20:04:41 +0100

    Kleinigkeiten

--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in {username}!')
+            messages.success(request, f'Dein Account wurde kreiert! Du kannst dich nun anmelden {username}!')
             return redirect('login')
     else:
         form = UserRegisterForm()
