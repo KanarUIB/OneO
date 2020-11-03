221ac8a36a356f82f9a1c0159399f4a681dc2ba6
cihadaydemir <cihadaydemir@live.de>
2020-11-03 12:10:19 +0100

    Login + Register Page incl. urls routing & settings

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,9 +18,14 @@ from django.urls import path, include
 from pages import views as page
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
     path('', page.home, name="index"),
     #path("", include("pages.urls"))
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('register/', user_views.register, name='register'),
+
 ]
