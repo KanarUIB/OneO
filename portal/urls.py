22eaecf8caca4b1a84f5d188ab87d9b80466f810
KanarUIB <1810275@stud.hs-mannheim.de>
2020-11-11 22:21:44 +0100

    twst

--- portal/urls.py
+++ portal/urls.py
@@ -23,9 +23,10 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
-    #path('', page.home, name="index"),
+    # path('', page.home, name="index"),
     path('', include("pages.urls")),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', user_views.register, name='register'),
     path('getUser/', page.getUser, name="getUser"),
 ]
