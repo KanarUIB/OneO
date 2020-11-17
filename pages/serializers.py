77b4efffde06f1e1a4ac86f700d3cc3a20af701e
KanarUIB <1810275@stud.hs-mannheim.de>
2020-11-17 12:48:59 +0100

    REST API

---
+++ pages/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from .models import Kunde, KundeHatSoftware, Software
+
+
+class KundenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Kunde
+        fields = '__all__'
+
+
+class KHSSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = KundeHatSoftware
+        fields = '__all__'
+
+
+class SoftwareSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Software
+        fields = '__all__'
+
--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name="index"),
     path('Kunden/Profil', views.kundenprofil, name="kundenprofil"),
     path('Kunden/', views.kunden, name="kunden"),
+    path('heartbeat', views.heartbeat, name="heartbeat"),
     #path('getUser/', views.getUser, name='getUser'),
 ]
