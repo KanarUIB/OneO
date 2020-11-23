8c9e9ae9f19b01a2327116f7538bf3075ddc08f5
KanarUIB <1810275@stud.hs-mannheim.de>
2020-11-23 15:40:34 +0100

    dynamische Kundenliste

--- pages/views.py
+++ pages/views.py
@@ -16,18 +16,31 @@ def home(request):
 
 
 def kundenprofil(request):
-    context = {
-        "kunde": Kunde.objects.all(),
-        "software": Software.objects.all(),
-        "khs": KundeHatSoftware.objects.all()
+    id = request.GET.get('id', None)
 
+    kunde = KundeHatSoftware.objects.get(id=id)
+
+
+
+    context = {
+        "kdNr": str(kunde.standort.kunde.id),
+        "name": str(kunde.standort.name),
+        "email": str(kunde.standort.email),
+        "telNr": str(kunde.standort.telNr),
+        "software": str(kunde.software),
+        "plz": str(kunde.standort.plz),
+        "ort": str(kunde.standort.ort),
+        "strasse": str(kunde.standort.strasse),
+        "hausnr": str(kunde.standort.hausnr),
     }
+
+    print(kunde)
+
     return render(request, "kundenprofil.html", context)
 
 def kunden(request):
     context = {
-        "kunde": Kunde.objects.all()
-
+        "kunde": Kunde.objects.all(),
     }
     return render(request, "kunden.html", context)
 
@@ -35,12 +48,12 @@ def kunden(request):
 def getUser(request):
     kdNr = request.GET.get('kdNr', None)
 
-    kunde = Standort.objects.filter(id=kdNr)
+    kunde = KundeHatSoftware.objects.filter(id=kdNr)
     kundenliste = []
     for x in kunde:
         kundenliste.append({
-            "id": str(x.id),
-            "swId": str(x.name),
+            "software": str(x.software),
+            "name": str(x.standort.name),
             #"lizenz": str(x.lizenz),
             #"version": str(x.swId.version)
         })
