6777ec74fccd256472e9a4faa029dbfd0e8610f8
uib-behar <Bachelor123!>
2021-01-21 00:36:32 +0100

    urls.py Suche-Path entfernt

--- pages/urls.py
+++ pages/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('', views.home, name="index"),
     path('Kunden/Profil', views.kundenprofil, name="kundenprofil"),
     path('Kunden/', views.kunden, name="kunden"),
-    path('Suche/', views.suche, name="suche"),
     # path('getUser/', views.getUser, name='getUser'),
 ]
