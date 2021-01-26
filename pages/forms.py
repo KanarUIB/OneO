1d4e4d8fca7023491b5250bd2993cd1514c65cd0
cihadaydemir <cihadaydemir@live.de>
2021-01-26 17:03:12 +0100

    forms Dokumentation users views Dokumentation

--- pages/forms.py
+++ pages/forms.py
@@ -3,45 +3,75 @@ from .models import Kunde, Standort, KundeHatSoftware, Lizenz, Modul, Standortli
 
 
 class KundeCreateForms(forms.ModelForm):
+    """
+        Das KundeCreateForms ist die Forms zur Erstellung eines Kunden.
+        Es bildet das Model Kunde mit all seinen Attributen ab.
+
+        Parameters:
+            data: Das KundeCreateForm kann jegliche Daten aufnehmen, welche zu einem Kunden gehören.
+    """
+
     class Meta:
+        """
+            model :     Das Model, welches die Forms abbildet.
+            fields :    Die Attribute des Models die man in der Forms abfragen möchte -> '__all__' bildet alle Attributen des Modells ab
+        """
+
         model = Kunde
         fields = '__all__'
 
 
 class StandortCreateForms(forms.ModelForm):
+    """
+        Das StandortCreateForms ist die Forms zur Erstellung eines Standortes.
+        Es bildet das Model Standort mit all seinen Attributen ab.
+
+        Parameters:
+            data: Das StandortCreateForm kann jegliche Daten aufnehmen, welche zu einem Standort gehören.
+    """
+
     class Meta:
+        """
+            model :     Das Model, welches die Forms abbildet.
+            fields :    Die Attribute des Models die man in der Forms abfragen möchte -> '__all__' bildet alle Attributen des Modells ab
+        """
+
         model = Standort
-        fields = [
-            'kunde',
-            'name',
-            'plz',
-            'ort',
-            'strasse',
-            'hausnr',
-            'email',
-            'telNr',
-        ]
+        fields = '__all__'
 
 
 class SoftwareCreateForm(forms.ModelForm):
+    """
+        Das SoftwareCreateForms ist die Forms zur Erstellung eines KundeHatSoftware.
+        Es bildet das Model KundeHatSoftware mit all seinen Attributen ab.
+
+        Parameters:
+            data: Das SoftwareCreateForm kann jegliche Daten aufnehmen, welche zu einem KundeHatSoftware-Paket gehören.
+    """
+
     class Meta:
+        """
+            model :     Das Model, welches die Forms abbildet.
+            fields :    Die Attribute des Models die man in der Forms abfragen möchte -> '__all__' bildet alle Attributen des Modells ab
+        """
+
         model = KundeHatSoftware
         fields = '__all__'
 
 
-class ModulCreateForm(forms.ModelForm):
-    class Meta:
-        model = Modul
-        fields = '__all__'
-
-
 class StandortlizenzCreateForm(forms.ModelForm):
-    class Meta:
-        model = Standortlizenz
-        fields = '__all__'
+    """
+        Das StandortlizenzCreateForms ist die Forms zur Erstellung eines Standortlizenzes.
+        Es bildet das Model Standortlizenz mit all seinen Attributen ab.
 
+        Parameters:
+            data: Das StandortlizenzCreateForm kann jegliche Daten aufnehmen, welche zu einem Standortlizenzen gehören.
+    """
 
-class StandortlizenzUpdateForm(forms.ModelForm):
     class Meta:
+        """
+            model :     Das Model, welches die Forms abbildet.
+            fields :    Die Attribute des Models die man in der Forms abfragen möchte -> '__all__' bildet alle Attributen des Modells ab
+        """
         model = Standortlizenz
         fields = '__all__'
--- users/views.py
+++ users/views.py
@@ -3,17 +3,20 @@ from django.contrib import messages
 from .forms import UserRegisterForm
 
 
-"""""""""
-Wird aufgerufen wenn der Nutzer sich auf dem Management Portal registrieren möchte.
-Übergeben wird ein POST-Request mittels der Forms und in der Methode auf Richtigkeit geprüft.
-Bei erfolgreicher Prüfung wird der Benutzer in der Datenbank anlegt und auf die Loginseite weitergeleitet und kann sich erfolgreich einloggen.
-Bei fehlgeschlagener Prüfung wird der Benutzer aufgefordert die fehlerhaften Eingaben zu korrigieren/ergänzen.
-
-@return render 
-"""""""""
 
 
 def register(request):
+    """
+    Wird aufgerufen wenn der Nutzer sich auf dem Management Portal registrieren möchte.
+    Übergeben wird ein POST-Request mittels der Forms und in der Methode auf Richtigkeit geprüft.
+    Bei erfolgreicher Prüfung wird der Benutzer in der Datenbank anlegt und auf die Loginseite weitergeleitet und kann sich erfolgreich einloggen.
+    Bei fehlgeschlagener Prüfung wird der Benutzer aufgefordert die fehlerhaften Eingaben zu korrigieren/ergänzen.
+    Parameters:
+        request (WSGIRequest)   :   Nimmt einen "POST"-Befehl an, um die Forms 
+    Returns:
+        redirect (HttpResponseRedirect) :   Leitet User weiter auf die Loginseite.
+        render (HttpResponse)   :           Gibt die Registrierungsseite mit der Forms zur Registrierung aus.
+    """
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
